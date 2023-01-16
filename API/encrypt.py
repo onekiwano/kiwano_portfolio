b85c887bd7a14ce7495c1223f25508999012c51e
Emmanuel Calvet <emmanuel.calvet@usherbrooke.ca>
2023-01-16 08:39:03 -0500

    Add files via upload

---
+++ API/encrypt.py
@@ -0,0 +1,42 @@
+from cryptography.fernet import Fernet
+import numpy as np
+from os.path import exists, join
+
+# Lien: https://www.geeksforgeeks.org/how-to-encrypt-and-decrypt-strings-in-python/
+
+# generate a key for encryptio and decryption
+# You can use fernet to generate
+# the key or use random key generator
+# here I'm using fernet to generate key
+
+def encrypt_key(secret_key, key=None, path='', name='key_crypto_bot.npy'):
+    
+    if (key is None):
+        path_file = join(path, name)
+        if not exists(path_file):
+            key = Fernet.generate_key()
+            np.save(path_file, key)
+        else:
+            key = str(np.load(path_file))[1:]
+    
+    # Instance the Fernet class with the key
+    fernet = Fernet(key)
+    
+    # then use the Fernet class instance
+    # to encrypt the string string must
+    # be encoded to byte string before encryption
+    encMessage = str(fernet.encrypt(secret_key.encode()))[1:]
+    decMessage = fernet.decrypt(encMessage.encode()).decode()
+    if decMessage != secret_key:
+        raise Exception('Issues when encrypting, source and decoded messages are not the same')
+    return encMessage
+
+def decrypt_key(encMessage, key=None, path=None, name='key_crypto_bot.npy'):
+    
+    if (key is None) and (path is not None):
+        path_file = join(path, name)
+        if exists(path_file):
+            key = str(np.load(path_file))[1:]    
+    fernet = Fernet(key)
+    decMessage = fernet.decrypt(encMessage.encode()).decode()
+    return decMessage
